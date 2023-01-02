c7e538e82984d5b6a2b9ed1efc3e4d3646d7da0f
Maarten de Jong <me@arteeh.com>
2023-01-02 19:56:00 +0100

    Remove unnecessaries

--- Binaries/Build Core.bat
+++
@@ -1,3 +0,0 @@
-rem D:\Git\PapyrusDotNet\Examples\Fallout4Example\bin\Debug\Fallout4Example.dll D:\Git\PapyrusDotNet\Source\..\Binaries\output\
-PapyrusDotNet.exe "D:\\Spel\\Fallout 4 Scripts\\scripts\\" "D:\\Git\\PapyrusDotNet\\Binaries\\" -clr 
-pause
